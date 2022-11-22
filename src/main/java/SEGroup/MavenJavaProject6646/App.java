c0daeabfe49d67ce6e9cd9099d1c1b0b64296068
Saikiran Reddy722 <mskreddy1618@gmail.com>
2022-11-23 00:15:25 +0530

    First Commit

---
+++ src/main/java/SEGroup/MavenJavaProject6646/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenJavaProject6646;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Saikiran Reddy!" );
+    }
+}
